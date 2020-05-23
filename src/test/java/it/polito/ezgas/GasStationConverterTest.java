b5f5ce75a3b00d8b00080ef2ce258db60955427c
Leonardo Izzi <s278564@studenti.polito.it>
2020-05-23 13:02:52 +0200

    test: adding Converter tests

---
+++ src/test/java/it/polito/ezgas/GasStationConverterTest.java
@@ -0,0 +1,67 @@
+package it.polito.ezgas;
+
+import it.polito.ezgas.converter.Converter;
+import it.polito.ezgas.converter.impl.GasStationConverter;
+import it.polito.ezgas.dto.GasStationDto;
+import it.polito.ezgas.entity.GasStation;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GasStationConverterTest {
+    @Test
+    public void testGasStationToDto() {
+        GasStation gasStation = new GasStation("Minmus", "Via Kerbal Space Program",
+                true, false, true, false, true, null,
+                10.32, -26.11, 1.1, -1, 1.2, -1, 1.3,
+                5, "10/12/20", 76);
+        gasStation.setGasStationId(1);
+        Converter<GasStation, GasStationDto> converter = new GasStationConverter();
+        GasStationDto gasStationDto = converter.convertToDto(gasStation);
+        assertEquals(1, gasStationDto.getGasStationId());
+        assertEquals("Minmus", gasStationDto.getGasStationName(), "dto name is wrong");
+        assertEquals("Via Kerbal Space Program", gasStationDto.getGasStationAddress(), "dto address is wrong");
+        assertTrue(gasStationDto.getHasDiesel(), "dto hasdiesel is wrong");
+        assertFalse(gasStationDto.getHasSuper(), "dto hassuper is wrong");
+        assertTrue(gasStationDto.getHasSuperPlus(), "dto hassuperplus is wrong");
+        assertFalse(gasStationDto.getHasGas(), "dto hasgas is wrong");
+        assertTrue(gasStationDto.getHasMethane(), "dto hasmethane is wrong");
+        assertNull(gasStationDto.getCarSharing(), "dto carsharing is wrong");
+        assertEquals(10.32, gasStationDto.getLat(), "dto latitude is wrong");
+        assertEquals(-26.11, gasStationDto.getLon(), "dto longitude is wrong");
+        assertEquals(1.1, gasStationDto.getDieselPrice(), "dto diesel price is wrong");
+        assertEquals(-1, gasStationDto.getSuperPrice(), "dto super price is wrong");
+        assertEquals(1.2, gasStationDto.getSuperPlusPrice(), "dto superplus price is wrong");
+        assertEquals(-1, gasStationDto.getGasPrice(), "dto gas price is wrong");
+        assertEquals(1.3, gasStationDto.getMethanePrice(), "dto methane price is wrong");
+        assertEquals("10/12/20", gasStationDto.getReportTimestamp(), "dto timestamp is wrong");
+        assertEquals(76, gasStationDto.getReportDependability(), "dto dependability is wrong");
+    }
+
+    @Test
+    public void testGasStationFromDto() {
+        GasStationDto gasStationDto = new GasStationDto(10, "Minmus", "Via Kerbal Space Program",
+                true, false, true, false, true, null,
+                10.32, -26.11, 1.1, -1, 1.2, -1, 1.3,
+                5, "10/12/20", 76);
+        Converter<GasStation, GasStationDto> converter = new GasStationConverter();
+        GasStation gasStation = converter.convertFromDto(gasStationDto);
+        assertEquals(10, gasStation.getGasStationId());
+        assertEquals("Minmus", gasStation.getGasStationName(), "entity name is wrong");
+        assertEquals("Via Kerbal Space Program", gasStation.getGasStationAddress(), "entity address is wrong");
+        assertTrue(gasStation.getHasDiesel(), "entity hasdiesel is wrong");
+        assertFalse(gasStation.getHasSuper(), "entity hassuper is wrong");
+        assertTrue(gasStation.getHasSuperPlus(), "entity hassuperplus is wrong");
+        assertFalse(gasStation.getHasGas(), "entity hasgas is wrong");
+        assertTrue(gasStation.getHasMethane(), "entity hasmethane is wrong");
+        assertNull(gasStation.getCarSharing(), "entity carsharing is wrong");
+        assertEquals(10.32, gasStation.getLat(), "entity latitude is wrong");
+        assertEquals(-26.11, gasStation.getLon(), "entity longitude is wrong");
+        assertEquals(1.1, gasStation.getDieselPrice(), "entity diesel price is wrong");
+        assertEquals(-1, gasStation.getSuperPrice(), "entity super price is wrong");
+        assertEquals(1.2, gasStation.getSuperPlusPrice(), "entity superplus price is wrong");
+        assertEquals(-1, gasStation.getGasPrice(), "entity gas price is wrong");
+        assertEquals(1.3, gasStation.getMethanePrice(), "entity methane price is wrong");
+        assertEquals("10/12/20", gasStation.getReportTimestamp(), "entity timestamp is wrong");
+        assertEquals(76, gasStation.getReportDependability(), "entity dependability is wrong");
+    }
+}
---
+++ src/test/java/it/polito/ezgas/UserConverterTest.java
@@ -0,0 +1,90 @@
+package it.polito.ezgas;
+
+import it.polito.ezgas.converter.Converter;
+import it.polito.ezgas.converter.impl.UserConverter;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserConverterTest {
+    @Test
+    public void testAdminConvertToDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        User user = new User("admin", "adminpw", "admin@ezgas.com", 5);
+        user.setAdmin(true);
+        user.setUserId(1);
+        UserDto userDto = converter.convertToDto(user);
+        assertEquals(1, userDto.getUserId(), "admin dto userdId is wrong");
+        assertEquals("admin", userDto.getUserName(), "admin dto username is wrong");
+        assertEquals("adminpw", userDto.getPassword(), "admin dto password is wrong");
+        assertEquals("admin@ezgas.com", userDto.getEmail(), "admin dto email is wrong");
+        assertEquals(5, userDto.getReputation(), "admin dto reputation is wrong");
+        assertTrue(userDto.getAdmin(), "admin dto admin is wrong");
+    }
+
+    @Test
+    public void testFirstConstructorAdminConvertFromDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        UserDto userDto = new UserDto(1, "admin", "adminpw", "admin@ezgas.com", 5);
+        userDto.setAdmin(true);
+        User user = converter.convertFromDto(userDto);
+        assertEquals("admin", user.getUserName(), "admin username is wrong");
+        assertEquals("adminpw", user.getPassword(), "admin password is wrong");
+        assertEquals("admin@ezgas.com", user.getEmail(), "admin email is wrong");
+        assertEquals(5, user.getReputation(), "admin reputation is wrong");
+        assertTrue(user.getAdmin(), "admin admin is wrong");
+    }
+
+    @Test
+    public void testSecondConstructorAdminConvertFromDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        UserDto userDto = new UserDto(1, "admin", "adminpw", "admin@ezgas.com", 5, true);
+        User user = converter.convertFromDto(userDto);
+        assertEquals("admin", user.getUserName(), "admin username is wrong");
+        assertEquals("adminpw", user.getPassword(), "admin password is wrong");
+        assertEquals("admin@ezgas.com", user.getEmail(), "admin email is wrong");
+        assertEquals(5, user.getReputation(), "admin reputation is wrong");
+        assertTrue(user.getAdmin(), "admin admin is wrong");
+    }
+
+    @Test
+    public void testUserConvertToDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        User user = new User("deadpool", "diehard", "deadpool@marvel.com", -3);
+        user.setAdmin(false);
+        user.setUserId(10);
+        UserDto userDto = converter.convertToDto(user);
+        assertEquals(10, userDto.getUserId(), "user dto userdId is wrong");
+        assertEquals("deadpool", userDto.getUserName(), "user dto username is wrong");
+        assertEquals("diehard", userDto.getPassword(), "user dto password is wrong");
+        assertEquals("deadpool@marvel.com", userDto.getEmail(), "user dto email is wrong");
+        assertEquals(-3, userDto.getReputation(), "user dto reputation is wrong");
+        assertFalse(userDto.getAdmin(), "user dto admin is wrong");
+    }
+
+    @Test
+    public void testFirstConstructorUserConvertFromDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        UserDto userDto = new UserDto(14, "Alter Bridge", "metalingus", "ab@rock.com", 4);
+        userDto.setAdmin(false);
+        User user = converter.convertFromDto(userDto);
+        assertEquals("Alter Bridge", user.getUserName(), "user username is wrong");
+        assertEquals("metalingus", user.getPassword(), "user password is wrong");
+        assertEquals("ab@rock.com", user.getEmail(), "user email is wrong");
+        assertEquals(4, user.getReputation(), "user reputation is wrong");
+        assertFalse(user.getAdmin(), "user admin is wrong");
+    }
+
+    @Test
+    public void testSecondConstructorUserConvertFromDto() {
+        Converter<User, UserDto> converter = new UserConverter();
+        UserDto userDto = new UserDto(1, "Jebedaiah Kerman", "MunCrash", "jeb@ksp.com", 0, false);
+        User user = converter.convertFromDto(userDto);
+        assertEquals("Jebedaiah Kerman", user.getUserName(), "user username is wrong");
+        assertEquals("MunCrash", user.getPassword(), "user password is wrong");
+        assertEquals("jeb@ksp.com", user.getEmail(), "user email is wrong");
+        assertEquals(0, user.getReputation(), "user reputation is wrong");
+        assertFalse(user.getAdmin(), "user admin is wrong");
+    }
+}
