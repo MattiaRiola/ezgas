646ad2c92327fb5ccde122598796d1d217c3b061
Leonardo Izzi <s278564@studenti.polito.it>
2020-05-23 16:43:19 +0200

    fix: order of query parameters fixed

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -31,18 +31,18 @@ public interface GasStationRepository extends JpaRepository<GasStation, Integer>
     @Query(value = "SELECT * FROM GAS_STATION WHERE CAR_SHARING = ?1", nativeQuery = true)
     List<GasStation> findByCarSharing (String carSharing);
 
-    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_DIESEL=TRUE AND CAR_SHARING = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_DIESEL=TRUE AND CAR_SHARING = ?1", nativeQuery = true)
     List<GasStation> findByHasDieselAndCarSharing(String carSharing);
 
-    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_GAS = TRUE AND CAR_SHARING=?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_GAS = TRUE AND CAR_SHARING=?1", nativeQuery = true)
     List<GasStation> findByGasAndCarSharing(String carSharing);
 
-    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_METHANE = TRUE AND CAR_SHARING=?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_METHANE = TRUE AND CAR_SHARING=?1", nativeQuery = true)
     List<GasStation> findByMethaneAndCarSharing(String carSharing);
 
-    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_SUPER = TRUE AND CAR_SHARING=?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_SUPER = TRUE AND CAR_SHARING=?1", nativeQuery = true)
     List<GasStation> findBySuperAndCarSharing(String carSharing);
 
-    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_SUPER_PLUS = TRUE AND CAR_SHARING=?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM GAS_STATION WHERE HAS_SUPER_PLUS = TRUE AND CAR_SHARING=?1", nativeQuery = true)
     List<GasStation> findBySuperPlusAndCarSharing(String carSharing);
 }
