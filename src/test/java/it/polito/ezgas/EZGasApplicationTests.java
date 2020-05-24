3390a09eb87116692f71b56751c2175d8cc3be7b
Leonardo Izzi <s278564@studenti.polito.it>
2020-05-24 12:02:08 +0200

    test: added repository tests in EZGasApplicationTests

--- src/test/java/it/polito/ezgas/EZGasApplicationTests.java
+++ src/test/java/it/polito/ezgas/EZGasApplicationTests.java
@@ -1,5 +1,7 @@
 package it.polito.ezgas;
 
+import it.polito.ezgas.repository.GasStationRepository;
+import it.polito.ezgas.repository.UserRepository;
 import org.junit.Test;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
@@ -8,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class) 
 @SpringBootTest
-@SelectClasses({GasStationEntityTest.class , UserEntityTest.class, HaversineTest.class, GasStationConverterTest.class, UserConverterTest.class, UserServiceTest.class, GasStationServiceTestMock.class})
+@SelectClasses({GasStationEntityTest.class , UserEntityTest.class, HaversineTest.class, GasStationConverterTest.class, UserConverterTest.class, GasStationRepository.class, UserRepository.class, UserServiceTest.class, GasStationServiceTestMock.class})
 public class EZGasApplicationTests {	
 	
 	@Test
