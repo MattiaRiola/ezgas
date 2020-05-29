58457449360e4ffbe72ef44f32ec556adf3ca50b
Leonardo Izzi <s278564@studenti.polito.it>
2020-05-29 19:46:02 +0200

    test: renamed folder to match lecture's requirements

--- src/test/java/it/polito/ezgas/controllertests/UserControllerTest.java
+++ src/test/java/it/polito/ezgas/controllertests/UserControllerTest.java
@@ -1,4 +1,4 @@
-package it.polito.ezgas.restapi;
+package it.polito.ezgas.controllertests;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
