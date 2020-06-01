64fbfd5d389c6d8d5abae61a36a6dbe1d2d3929f
Razieh Meidanshah- s274070 <s274070@studenti.polito.it>
2020-06-01 12:38:25 +0200

    Fix: GuiTests (1,3,4,6)

--- GUItests/gt-uc6.sikuli/gt-uc6.py
+++ GUItests/gt-uc6.sikuli/gt-uc6.py
@@ -1,7 +1,15 @@
 click("1590932987096.png")
 wait(1)
-type(Key.PAGE_DOWN)
-find("1590933260015.png")
-find("1590870823810.png")
-click("1590870830989.png")
+type(Key.PAGE_DOWN*3)
+wait("1591007190504.png",1)
+find("1591006840468.png")
+
+
+region = exists("1591006855216.png",1)
+
+if region:
+    region.click("1591006879616.png")
+    popup("Test Success!","test")
+else:
+    popup("Gas Station not found!","test")
 
